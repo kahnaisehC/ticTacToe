6b5584fe2a0062ee6cdcc04bc880cdb18a3b9b4d
Ian Hakanson <gabianhak@gmail.com>
2024-10-01 00:59:19 -0300

    feat: add check condition for vertical and horizontal wins

--- main.py
+++ main.py
@@ -43,14 +43,15 @@ def create_grid(n):
 class Game:
     def __init__(self, size, lineSize):
         self._size = size 
-        self._lineSize = lineSize
+        self._line_size = lineSize
         self._grid = create_grid(size)
         self._current_turn = 0
 
     def display_grid(self):
-        for col in self._grid:
-            for j in col:
-                print(j+" ", end="")
+        n = len(self._grid)
+        for i in range(0, n):
+            for j in range(0, n):
+                print(self._grid[i][j]+" ", end="")
             print()
 
     def declare_move(self, x:int, y:int):
@@ -75,15 +76,51 @@ class Game:
         self._current_turn += 1
 
     def check(self):
+        n = len(self._grid)
+        # check horizontal
+        for col in self._grid:
+            count = 1
+            for i in range(1, n):
+                if col[i] == '_':
+                    count = 1
+                    continue
+                if col[i] == col[i-1]:
+                    count += 1
+                else:
+                    count = 1
+                if count >= self._line_size:
+                    return False
+        
+        # check vertical
+        for y in range(0, n):
+            count = 1
+            for x in range(1, n):
+                if self._grid[x][y] == '_':
+                    count = 1
+                    continue
+                if self._grid[x][y] == self._grid[x-1][y]:
+                    count += 1
+                else:
+                    count = 1
+                if count >= self._line_size:
+                    return False
+        
+        #check diagonal
+        
+
+            
         return True
 
     def play(self):
+        winner = ""
         while(self.check()):
             self.display_grid()
-
-            x = input("Input the x coordinate: ")
-            y = input("Input the y coordinate: ")
+            x = int(input("Input the x coordinate: "))
+            y = int(input("Input the y coordinate: "))
             self.declare_move(int(x), int(y))
+            winner = self._grid[x][y]
+        self.display_grid()
+        print("THE WINNER IS: " + winner + "!!!!!!")
 
 
         
